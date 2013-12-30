29f05450f511bdbc6a3634d6332cce1fa5c2fcec
Soroush Pour <me@soroushjp.com>
2013-12-31 00:16:25 +1100

    Cleaned up blinking ACT LED code

--- source/main.s
+++ source/main.s
@@ -10,31 +10,32 @@ mov r1,#1
 lsl r1,#18
 str r1,[r0,#4]
 
-# Turn off GPIO Pin 16 output (to turn on ACT LED)
-turnon$:
 mov r1,#1
 lsl r1,#16
-str r1,[r0,#40]
 
-# Wait an arbitrary amount of time by subtracting 1 per cycle from 0x3F0000
-mov r2,#0x3F0000
+flashloop$:
 
-wait1$:
-sub r2,#1
-cmp r2,#0
-bne wait1$
+	# Turn off GPIO Pin 16 output (to turn on ACT LED)
+	str r1,[r0,#40]
 
-# Turn on GPIO Pin 16 output (to turn off ACT LED)
-turnoff$:
-str r1,[r0,#28]
+	# Wait an arbitrary amount of time by subtracting 1 from 0x3F0000 per cycle 
+	mov r2,#0x3F0000
 
-# Wait an arbitrary amount of time by subtracting 1 per cycle from 0x3F0000
-mov r2,#0x3F0000
+	wait1$:
+		sub r2,#1
+		cmp r2,#0
+		bne wait1$
 
-wait2$:
-sub r2,#1
-cmp r2,#0
-bne wait2$
+	# Turn on GPIO Pin 16 output (to turn off ACT LED)
+	str r1,[r0,#28]
 
-# Start at top with LED turn on again
-b turnon$
+	# Wait an arbitrary amount of time by subtracting 1 from 0x3F0000 per cycle 
+	mov r2,#0x3F0000
+
+	wait2$:
+		sub r2,#1
+		cmp r2,#0
+		bne wait2$
+
+	# Start at top with LED turn on again
+	b flashloop$
